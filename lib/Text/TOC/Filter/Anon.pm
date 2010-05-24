a1bb26a24150cdc8a30e1da71943bf1cb9ab9b16
Dave Rolsky <autarch@urth.org>
2010-05-24 11:11:41 -0500

    Docs for Anon filter

--- lib/Text/TOC/Filter/Anon.pm
+++ lib/Text/TOC/Filter/Anon.pm
@@ -10,9 +10,10 @@ use Moose;
 
 with 'Text::TOC::Role::Filter';
 
-has code => (
+has _code => (
     is       => 'ro',
     isa      => CodeRef,
+    init_arg => 'code',
     required => 1,
 );
 
@@ -20,9 +21,42 @@ sub node_is_interesting {
     my $self = shift;
     my $node = shift;
 
-    return $self->code()->($node);
+    return $self->_code()->($node);
 }
 
 __PACKAGE__->meta()->make_immutable();
 
 1;
+
+# ABSTRACT: Node filter wrapper for subroutine references
+
+=for Pod::Coverage
+    node_is_interesting
+
+=head1 SYNOPSIS
+
+  my $filter = Text::TOC::Filter::Anon->new( code => sub {...} );
+
+  if ( $filter->node_is_interesting($node) ) {...}
+
+=head1 DESCRIPTION
+
+This class wraps a subroutine reference with the Filter role API.
+
+=head1 METHODS
+
+This class provides the following methods:
+
+=head2 Text::TOC::Filter::Anon->new()
+
+This method accepts one parameter, C<code>, which must be a subroutine
+reference. That subroutine will be called with a single argument, some sort of
+node object. The specific type of object will depend on the type of source
+document being examined.
+
+=head1 ROLES
+
+This class implements the L<Text::TOC::Role::Filter> role.
+
+=cut
+
